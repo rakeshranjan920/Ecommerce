c79c9e8479052169a97877b2a8a521646c7338b0
rakeshranjan920 <rakeshranjan.iiita@gmail.com>
2021-01-30 19:07:21 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
 import userRouter from './routers/userRouter.js';
-
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL|| 'mongodb://localhost/Ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-// const MONGO_URI = 'mongodb+srv://rakeshranjan920:bim2014004_123@cluster0.42gca.mongodb.net/assignment?retryWrites=true&w=majority'
-// const connectDB = async () => {
-//     await mongoose.connect(MONGO_URI, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       useUnifiedTopology: true
-//     }).then((connect) => console.log('MongoDB connected: ', connect.connection.host));
-//   };
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -35,9 +26,8 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
